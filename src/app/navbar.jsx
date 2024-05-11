779b3fdd17610c4ed228cc2e0f47fb7134511bd2
Jean Osco <willjean29@gmail.com>
2024-05-10 23:20:07 -0500

    feat(auth): config auth with cognito and amplify

--- src/app/navbar.jsx
+++ src/app/navbar.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
+import { Hub } from 'aws-amplify/utils';
 
+import { getCurrentUser } from 'aws-amplify/auth';
 const data = [
   { url: '/', title: 'Home' },
   { url: '/create-post', title: 'Create Post' },
@@ -8,6 +11,29 @@ const data = [
 ];
 
 const Navbar = () => {
+  const [signedUser, setSignedUser] = useState(false);
+  useEffect(() => {
+    authListener();
+  }, [])
+
+  const authListener = async () => {
+    Hub.listen('auth', (data) => {
+      switch (data.payload.event) {
+        case 'signedIn':
+          setSignedUser(true);
+          break;
+        case 'signedOut':
+          setSignedUser(false);
+          break;
+      }
+    });
+    try {
+      await getCurrentUser();
+      setSignedUser(true);
+    } catch (err) {
+      setSignedUser(false);
+    }
+  };
   return (
     <nav
       className='flex justify-center pt-3 pb-3
@@ -20,6 +46,16 @@ const Navbar = () => {
           {title}
         </Link>
       ))}
+      {
+        signedUser && (
+          <Link href="/my-posts">
+            <a className='rounded-lg px-3 py-2 text-slate-700
+              font-medium hover:bg-slate-100 hover:text-slate-900'>
+              My Posts
+            </a>
+          </Link>
+        )
+      }
     </nav>
   )
 }

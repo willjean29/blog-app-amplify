15943cdbd480bf3fe855aabb145c8988aaa4ea21
Jean Osco <willjean29@gmail.com>
2024-05-12 00:04:36 -0500

    feat(auth): add sign out button

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -15,17 +15,17 @@ function Profile() {
       const currentSession = await fetchUserAttributes();
       setSession(currentSession);
     } catch (err) {
-      // setUser(null);
     }
   }
-  // if (!user) return null;
+  if (!session) return null;
   return (
     <Authenticator>
-      {({ user }) => (
+      {({ user, signOut }) => (
         <div>
           <h1 className="text-3xl font-semibold tracking-wide mt-6 ">Profile</h1>
-          <h1 className="font-medium text-gray-500 my-2">{user.username}</h1>
-          <h1 className="font-medium text-gray-500 my-2">{session?.email}</h1>
+          <h1 className="font-medium text-gray-500 my-2">Username: {user.username}</h1>
+          <h1 className="text-sm text-gray-500 m b-6">Email: {session?.email}</h1>
+          <button onClick={signOut}>Sign out</button>
         </div>
       )}
     </Authenticator>

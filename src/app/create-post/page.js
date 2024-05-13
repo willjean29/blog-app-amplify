b824aa446e7bcf057a96f545d36db1f9402d2f47
Jean Osco <willjean29@gmail.com>
2024-05-13 13:19:01 -0500

    feat(posts): add input file and preview to cover post

--- src/app/create-post/page.js
+++ src/app/create-post/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { v4 as uuid } from "uuid";
 import { client } from "../amplify-config";
 import { createPost } from '../../graphql/mutations';
@@ -7,11 +7,14 @@ import { useRouter } from "next/navigation";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { Authenticator } from "@aws-amplify/ui-react";
+import Image from "next/image";
 export default function CreatePost() {
   const [newPost, setNewPost] = useState({
     title: '',
     content: ''
   })
+  const [image, setImage] = useState(null);
+  const imageFileInput = useRef(null);
   const { title, content } = newPost;
   const router = useRouter();
   const onChange = (e) => {
@@ -41,9 +44,17 @@ export default function CreatePost() {
     } catch (error) {
       console.log({ error })
     }
-
   }
 
+  const uploadImage = () => {
+    imageFileInput.current.click();
+  }
+  const handleChange = (e) => {
+    const fileUploaded = e.target.files[0];
+    if (!fileUploaded) return;
+    setImage(fileUploaded);
+  }
+  console.log({ image })
   return (
     <Authenticator>
       {() => (
@@ -56,10 +67,22 @@ export default function CreatePost() {
             placeholder="Title"
             className="border-b pb-2 text-lg my-4 focus:outline-none w-full text-gray-500"
           />
+          {
+            image && (
+              <Image className="my-4" src={URL.createObjectURL(image)} alt="image cover to post" width={300} height={300} />
+            )
+          }
           <SimpleMDE
             value={content}
             onChange={(value) => setNewPost({ ...newPost, content: value })}
           />
+          <input
+            type="file"
+            ref={imageFileInput}
+            className="absolute w-0 h-0"
+            onChange={handleChange}
+          />
+          <button type="button" onClick={() => uploadImage()} className="mb-4 bg-green-600 text-white font-semibold px-8 py-2 rounded-lg mr-2">Upload Cover</button>
           <button type="button" onClick={createNewPost} className="mb-4 bg-blue-600 text-white font-semibold px-8 py-2 rounded-lg ">Create Post</button>
         </div>
       )}
